8c1e4197fa4779916de920295708923c044d2008
Laís <laisdeoliveira1215@gmail.com>
2025-09-26 12:59:18 -0300

    tela home

---
+++ drawer/src/home.js
@@ -0,0 +1,7 @@
+function HomeScreen() {
+    return (
+        <View style={{ flex:1, justifyContent:'center', alignItems: 'center'}}>
+            <Text>TELA HOME</Text>
+        </View>
+    );
+}
