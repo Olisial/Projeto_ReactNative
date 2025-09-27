7ef752da30728711853722a512ff5d33196e4439
Laís <laisdeoliveira1215@gmail.com>
2025-09-26 21:32:13 -0300

    tela home e perfil

--- drawer/app.js
+++ drawer/app.js
@@ -1,9 +1,24 @@
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
-import 'react-native-gesture-handler';
+import { Text, View } from 'react-native';
 
-import HomeScreen from './src/home';
-import ProfileScreen from './src/profile';
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+       <Text>TELA HOME</Text>
+      <Text>Mereço um 10 em, nos dois bimestres já 🥹😂</Text>
+    </View>
+  );
+}
+
+function ProfileScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+       <Text>TELA PERFIL</Text>
+      <Text>Laís Linda</Text>
+    </View>
+  );
+}
 
 const Drawer = createDrawerNavigator();
 
--- drawer/src/profile.js
+++
@@ -1,8 +0,0 @@
-function ProfileScreen() {
-    return (
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            <Text> TELA PERFIL</Text>
-            <Text> Laís Linda</Text>
-        </View>
-    );
-}
